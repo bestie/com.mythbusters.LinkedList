00f53bc8f9f89c1a967f08dce0eb2d07ea2d4a92
Stephen Best <bestie@gmail.com>
2015-11-28 16:01:41 +0000

    Skeleton linkedlist implementation.

---
+++ src/com/example/mythbusters/LinkedListTest.java
@@ -0,0 +1,15 @@
+package com.example.mythbusters;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class LinkedListTest {
+
+	@Test
+	public void test() {
+		LinkedList<Float> myList = new LinkedList<Float>();
+		fail("Not yet implemented");
+	}
+
+}
