7ca49d5fb84a779507c5bbc40ab236ab6b2f77d6
Stephen Best <bestie@gmail.com>
2015-11-28 22:36:34 +0000

    Add template listiterator impl.

--- src/com/example/mythbusters/LinkedList.java
+++ src/com/example/mythbusters/LinkedList.java
@@ -227,7 +227,62 @@ public class LinkedList<T> implements List<T> {
 
 	@Override
 	public ListIterator<T> listIterator() {
-		return null;
+		return new ListIterator<T>() {
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public T next() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public boolean hasPrevious() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public T previous() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public int nextIndex() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public int previousIndex() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public void remove() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void set(T e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void add(T e) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 	}
 
 	@Override
