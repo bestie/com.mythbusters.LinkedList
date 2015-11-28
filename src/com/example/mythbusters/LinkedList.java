403e4d77f525ef0c61eab3536438886be0ca1f65
Stephen Best <bestie@gmail.com>
2015-11-28 22:36:00 +0000

    Most of the implementation is done

--- src/com/example/mythbusters/LinkedList.java
+++ src/com/example/mythbusters/LinkedList.java
@@ -1,5 +1,6 @@
 package com.example.mythbusters;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -19,70 +20,47 @@ public class LinkedList<T> implements List<T> {
 			mNext = next;
 		}
 
-		public Node<T> getNext() {
-			return mNext;
-		}
-
 		public T getElement() {
 			return mElement;
+		}
+
+		public Node<T> getNext() {
+			return mNext;
 		}
 
 		public void setNext(Node<T> newNode) {
 			mNext = newNode;
 		}
+
+		public void setElement(T element) {
+			mElement = element;
+		}
+	}
+
+	private interface NodeCallback<A, T> {
+		public A call(A accumulator, LinkedList<T>.Node<T> currentNode);
 	}
 
 	private Node<T> mHead = null;
 
-	@Override
-	public int size() {
-		NodeCallback<Integer, T> func = new NodeCallback<Integer, T>() {
+	public LinkedList() {
 
-			@Override
-			public Integer call(Integer accumulator, LinkedList<T>.Node<T> currentNode) {
-				return accumulator + 1;
-			}
+	}
 
-		};
-
-		return reduceNodes(0, func);
+	public LinkedList(LinkedList<T> other) {
+		for (int i = 0; i < other.size(); i++) {
+			T valueToCopy = other.get(i);
+			add(valueToCopy);
+		}
 	}
 
 	@Override
-	public boolean isEmpty() {
-		return size() == 0;
-	}
+	public void add(int index, T element) {
+		Node<T> prevNode = getNodeByIndex(index - 1);
+		Node<T> nextNode = prevNode.getNext();
 
-	@Override
-	public boolean contains(Object element) {
-		NodeCallback<Boolean, T> containsElement = new NodeCallback<Boolean, T>() {
-
-			@Override
-			public Boolean call(Boolean accumulator, Node<T> currentNode) {
-				return accumulator || currentNode.getElement().equals(element);
-			}
-			
-		};
-		
-		return reduceNodes(false, containsElement);
-	}
-
-	@Override
-	public Iterator<T> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <A> A[] toArray(A[] array) {
-		// TODO Auto-generated method stub
-		return null;
+		Node<T> newNode = new Node<T>(element, nextNode);
+		prevNode.setNext(newNode);
 	}
 
 	@Override
@@ -108,87 +86,50 @@ public class LinkedList<T> implements List<T> {
 
 		return true;
 	}
-	
-
-	@Override
-	public void add(int index, T element) {
-		Node<T> prevNode = getNodeByIndex(index - 1);
-		Node<T> nextNode = prevNode.getNext();
-		
-		Node<T> newNode = new Node<T>(element, nextNode);
-		prevNode.setNext(newNode);
-	}
-
-	@Override
-	public boolean remove(Object element) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> elements) {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 	@Override
 	public boolean addAll(Collection<? extends T> elements) {
-		// TODO Auto-generated method stub
-		return false;
+		for (T element : elements) {
+			add(element);
+		}
+		return true;
 	}
 
 	@Override
 	public boolean addAll(int index, Collection<? extends T> elements) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		Node<T> insertionPoint = getNodeByIndex(index - 1);
+		Node<T> rest = getNodeByIndex(index);
 
-	@Override
-	public boolean removeAll(Collection<?> elements) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+		LinkedList<T> insertionElements = new LinkedList<T>();
+		insertionElements.addAll(elements);
 
-	@Override
-	public boolean retainAll(Collection<?> elements) {
-		// TODO Auto-generated method stub
-		return false;
+		insertionPoint.setNext(insertionElements.mHead);
+		Node<T> last = insertionElements.getNodeByIndex(insertionElements.size() - 1);
+		last.setNext(rest);
+
+		return true;
+
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		mHead = null;
 	}
 
 	@Override
-	public int indexOf(Object element) {
-		// TODO Auto-generated method stub
-		return 0;
+	public boolean contains(Object element) {
+		return indexOf(element) != -1;
 	}
 
 	@Override
-	public int lastIndexOf(Object element) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	public boolean containsAll(Collection<?> elements) {
+		for (Object element : elements) {
+			if (!contains(element)) {
+				return false;
+			}
+		}
 
-	@Override
-	public ListIterator<T> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ListIterator<T> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<T> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
+		return true;
 	}
 
 	@Override
@@ -202,15 +143,230 @@ public class LinkedList<T> implements List<T> {
 	}
 
 	@Override
-	public T set(int index, T element) {
+	public int indexOf(Object element) {
+		NodeCallback<Integer, T> indexSearcher = new NodeCallback<Integer, T>() {
+			int mCurrentIndex = 0;
+
+			@Override
+			public Integer call(Integer resultIndex, LinkedList<T>.Node<T> currentNode) {
+
+				Integer retVal = null;
+
+				if (currentNode.getElement().equals(element)) {
+					if (resultIndex == -1) {
+						retVal = mCurrentIndex;
+					}
+				}
+
+				if (retVal == null) {
+					retVal = resultIndex;
+				}
+
+				mCurrentIndex++;
+
+				return retVal;
+			}
+
+		};
+
+		return reduceNodes(-1, indexSearcher);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iterator<T>() {
+			
+			private Node<T> mNode = LinkedList.this.mHead;
+
+			@Override
+			public boolean hasNext() {
+				return mNode != null;
+			}
+
+			@Override
+			public T next() {
+				T retval = mNode.getElement();
+				mNode.getNext();
+				return retval;
+				
+			}
+		};
+		
+	}
+
+	@Override
+	public int lastIndexOf(Object element) {
+		NodeCallback<Integer, T> indexSearcher = new NodeCallback<Integer, T>() {
+			int mCurrentIndex = 0;
+
+			@Override
+			public Integer call(Integer resultIndex, LinkedList<T>.Node<T> currentNode) {
+
+				Integer retVal;
+
+				if (currentNode.getElement().equals(element)) {
+					retVal = mCurrentIndex;
+				} else {
+					retVal = resultIndex;
+				}
+
+				mCurrentIndex++;
+
+				return retVal;
+			}
+
+		};
+
+		return reduceNodes(-1, indexSearcher);
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		return null;
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int index) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public T remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (index == 0) {
+			T retval = mHead.getElement();
+			mHead = mHead.getNext();
+			return retval;
+		} else {
+
+			Node<T> prevNode = getNodeByIndex(index - 1);
+			Node<T> actual = prevNode.getNext();
+			Node<T> nextNode = prevNode.getNext().getNext();
+
+			prevNode.setNext(nextNode);
+			return actual.getElement();
+		}
+	}
+
+	@Override
+	public boolean remove(Object element) {
+		if (contains(element)) {
+			remove(indexOf(element));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> elements) {
+
+		int initialSize = size();
+
+		for (Object element : elements) {
+			remove(element);
+		}
+
+		int sizeAfter = size();
+
+		return initialSize != sizeAfter;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> elements) {
+		int initialSize = size();
+		LinkedList<T> copy = new LinkedList<T>(this);
+		while (copy.mHead != null && !elements.contains(copy.mHead.getElement())) {
+			copy.mHead = copy.mHead.getNext();
+		}
+
+		Node<T> prevNode = copy.mHead;
+		Node<T> currentNode = copy.mHead.getNext();
+		while (currentNode != null) {
+			if (!elements.contains(currentNode)) {
+				prevNode.setNext(currentNode.getNext());
+			} else {
+				prevNode = currentNode;
+			}
+			currentNode = currentNode.getNext();
+		}
+
+		mHead = copy.mHead;
+		int sizeAfter = size();
+
+		return initialSize != sizeAfter;
+	}
+
+	@Override
+	public T set(int index, T element) {
+		Node<T> node = getNodeByIndex(index);
+		T retVal = node.getElement();
+		node.setElement(element);
+		return retVal;
+	}
+
+	@Override
+	public int size() {
+		NodeCallback<Integer, T> func = new NodeCallback<Integer, T>() {
+
+			@Override
+			public Integer call(Integer accumulator, LinkedList<T>.Node<T> currentNode) {
+				return accumulator + 1;
+			}
+
+		};
+
+		return reduceNodes(0, func);
+	}
+
+	@Override
+	public List<T> subList(int fromIndex, int toIndex) {
+		LinkedList<T> copy = new LinkedList<T>(this);
+
+		for (int i = 0; i < fromIndex; i++) {
+			copy.remove(0);
+		}
+
+		int lastPointer = toIndex - fromIndex - 1;
+
+		copy.removeAfterIndex(lastPointer);
+
+		return copy;
+
+	}
+
+	private void removeAfterIndex(int i) {
+		getNodeByIndex(i).setNext(null);
+	}
+
+	@Override
+	public Object[] toArray() {
+		Object[] result = new Object[size()];
+		Node<T> currentNode = mHead;
+		int i = 0;
+		while (currentNode != null) {
+			result[i] = get(i++);
+		}
+
+		return result;
+	}
+
+	@Override
+	public <A> A[] toArray(A[] array) {
+		A[] result = (A[]) new Object[size()];
+		Node<T> currentNode = mHead;
+		int i = 0;
+		while (currentNode != null) {
+			result[i] = (A) get(i++);
+		}
+
+		return result;
 	}
 
 	private Node<T> getNodeByIndex(int index) {
@@ -225,11 +381,11 @@ public class LinkedList<T> implements List<T> {
 					return accumulator;
 				}
 			}
-		}; 
-		
+		};
+
 		return reduceNodes(null, nodeRetriever);
 	}
-	
+
 	private <A> A reduceNodes(A accumulator, NodeCallback<A, T> callback) {
 		if (mHead == null) {
 			return accumulator;
@@ -244,9 +400,5 @@ public class LinkedList<T> implements List<T> {
 
 		return accumulator;
 
-	}
-
-	private interface NodeCallback<A, T> {
-		public A call(A accumulator, LinkedList<T>.Node<T> currentNode);
 	}
 }
--- src/com/example/mythbusters/LinkedListTest.java
+++ src/com/example/mythbusters/LinkedListTest.java
@@ -5,18 +5,20 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Test;
 
 public class LinkedListTest {
 	private LinkedList<String> emptyList() {
 		return listWithNItems(0);
-    }
-	
+	}
+
 	private String fabricateElementForIndex(int i) {
 		return "I am element " + i;
 	}
-	
+
 	private LinkedList<String> listWithNItems(int n) {
 		LinkedList<String> newList = new LinkedList<String>();
 		for (int i = 0; i < n; i++) {
@@ -24,22 +26,22 @@ public class LinkedListTest {
 		}
 		return newList;
 	}
-	
+
 	@Test
 	public void testNewListIsEmpty() {
 		assertTrue(emptyList().isEmpty());
 	}
-	
+
 	@Test
 	public void testListWithItemIsNotEmpty() {
 		assertFalse(listWithNItems(1).isEmpty());
 	}
-	
+
 	@Test
 	public void testListWithItemHasSizeOne() {
 		assertEquals(1, listWithNItems(1).size());
 	}
-	
+
 	@Test
 	public void testAddRandomItemsGivesCorrectLength() {
 		int itemCount = 2;
@@ -47,57 +49,57 @@ public class LinkedListTest {
 
 		itemCount = 35;
 		assertEquals(itemCount, listWithNItems(itemCount).size());
-		
+
 		itemCount = 1337;
 		assertEquals(itemCount, listWithNItems(itemCount).size());
 	}
-	
+
 	@Test
 	public void testListContainsAddedItems() {
 		int itemCount = 29;
 		int containingIndex = 16;
-		
+
 		LinkedList<String> newList = listWithNItems(itemCount);
-		
-		String element = fabricateElementForIndex(containingIndex);		
+
+		String element = fabricateElementForIndex(containingIndex);
 		assertTrue(newList.contains(element));
 	}
-	
+
 	@Test
 	public void testListContainsSpecialAddedItems() {
 		int itemCount = 29;
 		LinkedList<String> newList = listWithNItems(itemCount);
 		String element = "bees";
-		
+
 		int insertPosition = itemCount - 2;
 		newList.add(insertPosition, element);
-		
+
 		assertEquals(element, newList.get(insertPosition));
 	}
-	
+
 	@Test
 	public void testListDoesNotContainMissingItems() {
 		int itemCount = 29;
 		int nonContainingIndex = itemCount + 27;
-		
+
 		LinkedList<String> newList = listWithNItems(itemCount);
-		
+
 		String element = fabricateElementForIndex(nonContainingIndex);
 		assertFalse(newList.contains(element));
 	}
-	
+
 	@Test
 	public void testEmptyListDoesNotContainNotItems() {
 		assertFalse(emptyList().contains("nothing"));
 	}
-	
+
 	@Test
 	public void testGetReturnsNullForAbsentElement() {
 		int itemCount = 3;
 		int nonContainingIndex = itemCount + 57;
-		
+
 		LinkedList<String> newList = listWithNItems(itemCount);
-		
+
 		assertNull(newList.get(nonContainingIndex));
 	}
 
@@ -105,10 +107,201 @@ public class LinkedListTest {
 	public void testGetReturnsElementForPresentElement() {
 		int itemCount = 3;
 		int containingIndex = 2;
-		
+
 		LinkedList<String> newList = listWithNItems(itemCount);
-		
+
 		String element = fabricateElementForIndex(containingIndex);
 		assertEquals(element, newList.get(containingIndex));
+	}
+
+	@Test
+	public void testAddAll() {
+		List<String> elements = new ArrayList<String>();
+
+		elements.add("bees");
+		elements.add("faces");
+
+		LinkedList<String> list = emptyList();
+
+		list.addAll(elements);
+
+		for (String element : elements) {
+			assertTrue(list.contains(element));
+		}
+	}
+
+	@Test
+	public void testAddAllAtIndex() {
+		List<String> elements = new ArrayList<String>();
+
+		elements.add("bees");
+		elements.add("faces");
+
+		int count = 2;
+		int insertionPoint = 1;
+
+		LinkedList<String> list = listWithNItems(count);
+
+		list.addAll(insertionPoint, elements);
+
+		for (int i = 0; i < elements.size(); i++) {
+			assertEquals(elements.get(i), list.get(insertionPoint + i));
+		}
+
+		assertEquals(fabricateElementForIndex(1), list.get(list.size() - 1));
+	}
+
+	@Test
+	public void testClear() {
+		LinkedList<String> list = listWithNItems(10);
+		list.clear();
+		assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testContainsAll() {
+		List<String> elements = new ArrayList<String>();
+
+		elements.add("bees");
+		elements.add("faces");
+
+		LinkedList<String> list = emptyList();
+
+		list.addAll(elements);
+
+		assertTrue(list.containsAll(elements));
+	}
+
+	@Test
+	public void testIndexOf() {
+		LinkedList<String> list = listWithNItems(10);
+		int expectedIndex = 3;
+		String elementToFind = fabricateElementForIndex(expectedIndex);
+
+		assertEquals(expectedIndex, list.indexOf(elementToFind));
+
+	}
+
+	@Test
+	public void testLastIndexOf() {
+		LinkedList<String> list = listWithNItems(10);
+		int expectedIndex = 4;
+		String elementToFind = fabricateElementForIndex(expectedIndex);
+
+		list.add(elementToFind);
+
+		assertEquals(list.size() - 1, list.lastIndexOf(elementToFind));
+
+	}
+
+	@Test
+	public void testRemove() {
+		LinkedList<String> list = listWithNItems(10);
+		int removeIndex = 4;
+		String elementToBeRemoved = fabricateElementForIndex(removeIndex);
+
+		String returnVal = list.remove(removeIndex);
+
+		assertEquals(9, list.size());
+		assertEquals(elementToBeRemoved, returnVal);
+		assertFalse(list.contains(elementToBeRemoved));
+	}
+
+	@Test
+	public void testRemoveAtZero() {
+		LinkedList<String> list = listWithNItems(2);
+		int removeIndex = 0;
+		String elementToBeRemoved = fabricateElementForIndex(removeIndex);
+
+		String returnVal = list.remove(removeIndex);
+
+		assertEquals(1, list.size());
+		assertEquals(elementToBeRemoved, returnVal);
+		assertFalse(list.contains(elementToBeRemoved));
+	}
+
+	@Test
+	public void testRemoveWithElement() {
+		LinkedList<String> list = listWithNItems(3);
+		int removeIndex = 1;
+		String elementToBeRemoved = fabricateElementForIndex(removeIndex);
+
+		boolean returnVal = list.remove(elementToBeRemoved);
+
+		assertEquals(2, list.size());
+		assertTrue(returnVal);
+		assertFalse(list.contains(elementToBeRemoved));
+	}
+
+	@Test
+	public void testRemoveAll() {
+		List<String> elements = new ArrayList<String>();
+
+		elements.add("bees");
+		elements.add("faces");
+		elements.add("wasps");
+		elements.add("potatos");
+
+		LinkedList<String> list = emptyList();
+
+		list.add("wasps");
+		list.add("potatos");
+
+		list.addAll(elements);
+		boolean retVal = list.removeAll(elements);
+
+		assertEquals(2, list.size());
+		assertTrue(retVal);
+	}
+
+	@Test
+	public void testRemoveAllUnchages() {
+		List<String> elements = new ArrayList<String>();
+
+		elements.add("bees");
+
+		LinkedList<String> list = listWithNItems(10);
+		boolean retVal = list.removeAll(elements);
+
+		assertEquals(10, list.size());
+		assertFalse(retVal);
+	}
+
+	@Test
+	public void testRetainAll() {
+		List<String> elements = new ArrayList<String>();
+
+		elements.add("bees");
+
+		LinkedList<String> list = listWithNItems(10);
+		list.add("bees");
+		
+		boolean retVal = list.retainAll(elements);
+
+		assertEquals(1, list.size());
+		assertTrue(list.contains("bees"));
+		assertTrue(retVal);
+	}
+	
+	@Test
+	public void testSet() {
+		List<String> list = listWithNItems(10);
+		list.set(3,"bees");
+		
+		assertEquals("bees", list.get(3));
+	}
+	
+	@Test
+	public void testSubList() {
+		List<String> list = listWithNItems(10);
+		int fromIndex = 3;
+		int toIndex = 7;
+		
+		List<String> subList = list.subList(fromIndex, toIndex);
+
+		for (int i = fromIndex; i < toIndex; i++) {
+			String expected = fabricateElementForIndex(i);
+			assertTrue(subList.contains(expected));
+		}
 	}
 }
